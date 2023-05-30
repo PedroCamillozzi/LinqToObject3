0c8cecb3a92d3fa185349cbcb05b520fe408a13d
PedroCamillozzi <pedrocamillozzi01@gmail.com>
2023-05-30 18:54:16 -0300

    Agregar archivos de proyecto.

---
+++ Clases/Ciudad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToObject3.Clases
+{
+    internal class Ciudad
+    {
+        private string _nombre;
+        private int _codPostal;
+
+        public string Nombre { get; set; }
+        public int CodigoPostal { get; set; }
+
+        public Ciudad(string nombre, int cp)
+        {
+            this.Nombre = nombre;
+            this.CodigoPostal = cp;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using LinqToObject3.Clases;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace LinqToObject3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList ciudades = new ArrayList();
+
+            ciudades.Add(new Ciudad("Rosario", 2000));
+            ciudades.Add(new Ciudad("Firmat", 2630));
+            ciudades.Add(new Ciudad("Cañada Del Ucle", 2635));
+            ciudades.Add(new Ciudad("Casilda", 2170));
+            ciudades.Add(new Ciudad("Venado Tuerto", 2600));
+            ciudades.Add(new Ciudad("Chañar Ladeado", 2643));
+            ciudades.Add(new Ciudad("Santa Fe", 3000));
+            ciudades.Add(new Ciudad("Los Quirquincho", 2637));
+            ciudades.Add(new Ciudad("Berabevu", 2639));
+            ciudades.Add(new Ciudad("Villada", 2173));
+
+            Console.WriteLine("Ingrese la ciudad o como inicia");
+            string rta = Console.ReadLine().ToLower();
+
+            var cFiltradas = ciudades.Cast<Ciudad>().Where(c => c.Nombre.ToLower().StartsWith(rta)).Select(c => new { Nombre = c.Nombre, CodigoPostal = c.CodigoPostal });
+
+            if (cFiltradas.Count() == 0)
+            {
+                Console.WriteLine("No hay ciudades cargadas que empiecen asi");
+
+            }
+            else
+            {
+                foreach (var c in cFiltradas)
+                {
+                    Console.WriteLine($"{c.Nombre}  {c.CodigoPostal}");
+                }
+            }
+        }
+    }
+}
